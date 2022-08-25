a7f56b1f44eddd2f723c9a7e49a47b68e76d170f
Rajeev K Tomy <rajeevphpdeveloper@gmail.com>
2022-08-25 10:33:00 +0530

    Fix PaymentMethodFormContext import issue

--- src/hooks/useAmazonPayFormikContext.js
+++ src/hooks/useAmazonPayFormikContext.js
@@ -1,5 +1,6 @@
 import { useContext } from 'react';
-import PaymentMethodFormContext from '../../../../components/paymentMethod/context/PaymentMethodFormContext';
+
+import { PaymentMethodFormContext } from '../../../../app/code/paymentMethod/context';
 
 export default function useAmazonPayFormikContext() {
   return useContext(PaymentMethodFormContext);
